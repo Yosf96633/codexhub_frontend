6c5d76cd2031737d34e4212c96e4dba4a56c452c
Yosf96633 <yosf96633@gmail.com>
2025-08-05 22:52:19 +0500

    Update dependencies, enhance global styles, and implement new components

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const page = () => {
+const Hero = () => {
   return (
     <div>
-        
+         
     </div>
+
   )
 }
 
-export default page
+export default Hero
